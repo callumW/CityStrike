0c712dda7d71ec9b16a186ae7ceea9a26a0a9ff0
Callum Wilson <calwilson@live.co.uk>
2019-11-23 22:27:40 +0000

    Added houses and missile emitter that targets houses

--- 3DMissileCommand/GameViewController.swift
+++ 3DMissileCommand/GameViewController.swift
@@ -9,46 +9,72 @@
 import UIKit
 import QuartzCore
 import SceneKit
+import GameplayKit
 
-class GameViewController: UIViewController {
+func *(left: SCNVector3, right: Float) -> SCNVector3 {
+    return SCNVector3(x: left.x * right, y: left.y * right, z: left.z * right)
+}
+func -(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3(x: left.x - right.x, y: left.y - right.y, z: left.z - right.z)
+}
+
+func magnitude(_ vec: SCNVector3) -> Float {
+    let sqrSum = (vec.x * vec.x) + (vec.y * vec.y) + (vec.z * vec.z)
+    return sqrtf(sqrSum)
+}
+
+func normalise(_ vec: SCNVector3) -> SCNVector3 {
+    let mag: Float = magnitude(vec)
+    if (mag == 0) {
+        return vec
+    }
+    return SCNVector3(x: vec.x / mag, y: vec.y / mag, z: vec.z / mag)
+}
+
+class GameViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysicsContactDelegate {
     
-    var missileEmitterLocation = SCNVector3(x: -6, y: 2.5, z: 0)
+    let missileSpawnInterval: Double = 0.5
+    var lastMissileSpawn: Double = 0.0
+    let missileSpawnHeight:Double = 10
+    // let missileEmitterLocation = SCNVector3(x: -6, y: 2.5, z: 0)
     var missileNode:SCNNode!
+    var gameScene: SCNScene!
+    var randomDistribution: GKRandomDistribution!
+    
+    var houseList: Array<SCNNode> = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // create a new scene
-        let scene = SCNScene(named: "art.scnassets/Theatre.scn")!
+        gameScene = SCNScene(named: "art.scnassets/Theatre.scn")!
+        gameScene?.physicsWorld.contactDelegate = self
         
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
         
         // create and add a light to the scene
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
         lightNode.light!.type = .omni
         lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
+        gameScene.rootNode.addChildNode(lightNode)
         
         // create and add an ambient light to the scene
         let ambientLightNode = SCNNode()
         ambientLightNode.light = SCNLight()
         ambientLightNode.light!.type = .ambient
         ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
+        gameScene.rootNode.addChildNode(ambientLightNode)
         
         
         // retrieve the SCNView
         let scnView = self.view as! SCNView
         
         // set the scene to the view
-        scnView.scene = scene
+        scnView.scene = gameScene
+        
+        scnView.isPlaying = true
+        
+        scnView.delegate = self
         
         
         // show statistics such as fps and timing information
@@ -66,12 +92,16 @@ class GameViewController: UIViewController {
         
         // get our missile node
         missileNode = missileScene?.rootNode.childNode(withName: "missile", recursively: true)
+        missileNode.physicsBody?.contactTestBitMask = 2
         
-        scene.rootNode.addChildNode(missileNode)
+        // get our houses
+        for node in gameScene.rootNode.childNodes {
+            if node.name == "House reference" {
+                houseList.append(node)
+            }
+        }
         
-        missileNode.position = missileEmitterLocation
-//        missileNode.rotation = SCNVector4(
-        
+        randomDistribution = GKRandomDistribution(lowestValue: 0, highestValue: houseList.count - 1)
     }
     
     @objc
@@ -79,6 +109,62 @@ class GameViewController: UIViewController {
         // retrieve the SCNView
         // let scnView = self.view as! SCNView
         
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        // check if we need to create missile
+        // print("render")
+        if (lastMissileSpawn == 0) {
+            lastMissileSpawn = time
+        }
+        else if (time - lastMissileSpawn > missileSpawnInterval) {
+            lastMissileSpawn = time
+            // get random location along spawn pane
+            let spawnLocation = SCNVector3(x: -20, y: Float(missileSpawnHeight), z: 0)
+            // choose random target
+            
+            let houseIndex: Int = randomDistribution.nextInt()
+            var target:SCNVector3 = SCNVector3(0, 0, 0)
+            if (houseIndex < houseList.count) {
+                target = houseList[houseIndex].position
+            }
+            else {
+                print("index: \(houseIndex) is too big")
+                target = houseList[0].position
+            }
+            
+            // Get direction
+            let dir:SCNVector3 = normalise(target - spawnLocation)
+            let force = dir * Float(20.0)
+            
+            
+            print("Firing missile at \(target) from \(spawnLocation) (\(dir)) with force \(force)")
+            
+            // spawn and apply force
+            
+            let newMissile:SCNNode = missileNode.clone()
+            
+            newMissile.physicsBody?.applyForce(force, asImpulse: false)
+            newMissile.position = spawnLocation
+            newMissile.look(at: target, up: SCNVector3(1, 0, 0), localFront: SCNVector3(0, 1, 0))
+            
+            
+            
+            gameScene?.rootNode.addChildNode(newMissile)
+        }
+    }
+    
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        var contactNode: SCNNode!
+        print("Collision")
+        if (contact.nodeA.name == "missile") {
+            contactNode = contact.nodeA
+        }
+        else{
+            contactNode = contact.nodeB
+        }
+        
+        contactNode.removeFromParentNode()
     }
     
     override var shouldAutorotate: Bool {
